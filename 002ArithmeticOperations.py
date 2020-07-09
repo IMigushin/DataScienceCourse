bc9890f0794a3cb17580058c5c8495f753fc716d
IMigushin <imigushin@gmail.com>
2020-07-09 18:59:43 +0300

    Added Arithmetic practice (finished) Added Operations with variables Added Variable naming conventions (to be continued)

--- 002ArithmeticOperations.py
+++ 002ArithmeticOperations.py
@@ -7,6 +7,7 @@ print(2 ** 3)
 print(7 / 4)
 print(7 // 4)
 print(7 % 4)
+
 print("Base arithmetic operations between float:")
 print(2.3 + 7.7)
 print(2.3 - 7.7)
@@ -16,22 +17,5 @@ print(7.7 / 2.3)
 print(7.7 // 2.3)
 print(7.7 % 2.3)
 
-# Python3 have dynamic data type
-print("Base arithmetic expressions:")
-x = 5
-print(x)
-x = "Hello!"
-print(x)
 
-# How to know type of variables cause of dynamic type issues
-print("How to know type of variables cause of dynamic type issues:")
-x = "Hello!"
-type_of_variable = type(x)
-print(type_of_variable)
-x = 7
-type_of_variable = type(x)
-print(type_of_variable)
-x = 5.6
-type_of_variable = type(x)
-print(type_of_variable)
 
---
+++ 003OperationsWithVariables.py
@@ -0,0 +1,20 @@
+# Python3 have dynamic data type
+print("Base arithmetic expressions:")
+x = 5
+print(x)
+x = "Hello!"
+print(x)
+
+# How to know type of variables cause of dynamic type issues
+print("How to know type of variables cause of dynamic type issues:")
+x = "Hello!"
+type_of_variable = type(x)
+print(type_of_variable)
+x = 7
+type_of_variable = type(x)
+print(type_of_variable)
+x = 5.6
+type_of_variable = type(x)
+print(type_of_variable)
+
+
